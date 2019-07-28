cfa41f507eea96d178649eed59dfc7acc1fab3a8
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2019-07-27 23:07:29 -0500

    Wrote Happy Birthday in JavaScript

--- README.md
+++
@@ -1,1 +0,0 @@
-I developed this code on MacOS, however, I want Windows users to be able to run it.
--- draw-birthday-card.js
+++
@@ -1,4 +0,0 @@
-var myCanvas = document.getElementById("myCanvas");
-var myContext = myCanvas.getContext("2d");
-// myContext.fillStyle = "#FF0000";
-// myContext.fillRect(0, 0, 800, 600);

67253c8080a6965c7b39a3251d5c25a4d626314d
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-06-29 01:43:53 -0500

    Used Act to refactor action: act pull_request -e .github/actions/check-pr-title/wrong_title_payload.json --secret-file ~/Projects/hbd_repo_token.txt --action-offline-mode

--- .github/actions/check-pr-title/index.js
+++ .github/actions/check-pr-title/index.js
@@ -2,64 +2,35 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 function addCommentToPR(comment, issue_number, repo, owner) {
-    const retVal = {
-        status: true,
-        error: ''
-    };
-
-    try {
-        const octokit = github.getOctokit(core.getInput('gh_token'));
-        octokit.rest.issues.createComment({
-            owner,
-            repo,
-            issue_number,
-            body: comment
-        });
-    }
-    catch (error) {
-        retVal.status = false;
-        retVal.error = error.toString();
-    }
-
-    return retVal;
+    octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number,
+        body: comment
+    });
 }
 
 async function getTitleOfPR(number, repo, owner) {
-    const retVal = {
-        prTitle: '',
-        status: true,
-        error: ''
-    };
-
-    try {
-        const octokit = github.getOctokit(core.getInput('gh_token'));
-        const pr = await octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number: number
-        });
-        retVal.prTitle = pr.data.title;
-    }
-    catch (error) {
-        retVal.status = false;
-        retVal.error = error.toString();
-    }
-
-    return retVal;
+    const pr = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number: number
+    });
+    return pr.data.title;
 }
 
-const {owner, repo, number} = github.context.issue;
-const {prTitle, status, error} = await getTitleOfPR(number, repo, owner);
-const expectedPrefix = 'Jira-';
-if (status) {
+const octokit = github.getOctokit(core.getInput('gh_token'));
+try {
+    const {number, repo, owner} = github.context.issue;
+    const prTitle = await getTitleOfPR(number, repo, owner);
+    const expectedPrefix = 'Jira-';
     if (!prTitle.startsWith(expectedPrefix)) {
-        const {status, error} = addCommentToPR(
-            `Expected title to start with "${expectedPrefix}", however:\n${prTitle}`,
-            number, repo, owner);
-        if (!status) {
-            core.setFailed(error);
-        }
+        addCommentToPR(`Expected title to start with "${expectedPrefix}", however:\n${prTitle}`,
+                       number,
+                       repo,
+                       owner);
     }
-} else {
-    core.setFailed(error);
+}
+catch (error) {
+    core.setFailed(error.toString());
 }
